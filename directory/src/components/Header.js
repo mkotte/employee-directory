585ab4a044b7275fda3c870a72e31b007d5ed24a
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-23 15:46:17 -0400

    created the Header component, using bootstrap

---
+++ directory/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+    render() {
+        return (
+        <nav class="navbar navbar-light bg-light justify-content-between">
+            <a class="navbar-brand">Employee Directory</a>
+            <form class="form-inline">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </nav>
+        )
+    };
+};
+
