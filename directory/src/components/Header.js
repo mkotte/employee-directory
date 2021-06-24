98c44e68b378ee4296bc40ac66f9a035765b4ba8
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-24 01:58:57 -0400

    changed header and search from from class based to function based components, expecting to have to implement useState/useEffect

--- directory/src/components/Header.js
+++ directory/src/components/Header.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
+import SearchForm from "./SearchForm.js";
 
-export default class Header extends Component {
-    render() {
-        return (
-        <nav class="navbar navbar-light bg-light justify-content-between">
-            {/* <a class="navbar-brand" href=''>Employee Directory</a> */}
-            <form class="form-inline">
-              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </nav>
-        )
-    };
+function Header(){
+    return (
+    <nav class="navbar navbar-light bg-light justify-content-between">
+        <h1>Employee Directory</h1>
+        <SearchForm />
+    </nav>
+    );
 };
+
+export default Header;
 
